64c71123640f8049ede72e32063c91db829ce0f9
SonYoonSeok <dbstjr5517@naver.com>
2022-04-22 10:42:43 +0900

    :recycle: :: UserService -> MemberService 이름 변경

--- src/main/kotlin/dashmap/controller/UserController.kt
+++ src/main/kotlin/dashmap/controller/UserController.kt
@@ -1,10 +1,9 @@
 package dashmap.controller
 
-import dashmap.service.UserService
+import dashmap.service.MemberService
 import dashmap.web.response.AuthUserResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class UserController(
-    val userService: UserService
+    val memberService: MemberService
 ) {
 
     // GET https://github.com/login/oauth/authorize?client_id=e478d9cea0fa33472386
     @GetMapping("/login")
     suspend fun login(@RequestParam code: String): ResponseEntity<AuthUserResponse> {
-        return ResponseEntity.ok(userService.login(code))
+        return ResponseEntity.ok(memberService.login(code))
     }
 }
