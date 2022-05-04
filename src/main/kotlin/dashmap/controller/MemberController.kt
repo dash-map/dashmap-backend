9cf8156dd658ffcbb0c0085eb354693c1c2e0764
SonYoonSeok <dbstjr5517@naver.com>
2022-05-04 12:16:15 +0900

    :recycle: :: UserController -> MemberController

--- src/main/kotlin/dashmap/controller/MemberController.kt
+++ src/main/kotlin/dashmap/controller/MemberController.kt
@@ -6,15 +6,13 @@ import dashmap.web.response.UserResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class UserController(
+class MemberController(
     val memberService: MemberService
 ) {
 
